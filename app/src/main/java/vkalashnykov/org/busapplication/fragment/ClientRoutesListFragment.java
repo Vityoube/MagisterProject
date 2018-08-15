338fbe807a1b8728714c32cc01c14f6b07aaa936
vkalashnykov <vityoube@gmail.com>
2018-08-15 19:37:09 +0200

    Added List Fragment for displaying accessible Routes for Client. There is an issue, dedicated to Firebase UI, the library that adapts list of Routes from Database Reference

---
+++ app/src/main/java/vkalashnykov/org/busapplication/fragment/ClientRoutesListFragment.java
@@ -0,0 +1,32 @@
+package vkalashnykov.org.busapplication.fragment;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import vkalashnykov.org.busapplication.R;
+import vkalashnykov.org.busapplication.api.domain.Route;
+
+public class ClientRoutesListFragment extends ListFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_client_routes_list,null);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        DatabaseReference routesReference= FirebaseDatabase.getInstance().getReference().child("routes");
+//        FirebaseListAdapter<Route> adapter=
+
+    }
+}

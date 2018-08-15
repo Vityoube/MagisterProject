61d77ae50b643586821a5e7453aa0621df67c3a8
vkalashnykov <vityoube@gmail.com>
2018-08-15 17:27:27 +0200

    Added MapFragment for Main Client Activity

--- app/src/main/java/vkalashnykov/org/busapplication/ClientMapFragment.java
+++ app/src/main/java/vkalashnykov/org/busapplication/ClientMapFragment.java
@@ -1,18 +1,39 @@
 package vkalashnykov.org.busapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
+import vkalashnykov.org.busapplication.api.domain.Point;
 import vkalashnykov.org.busapplication.api.domain.Route;
 
 public class ClientMapFragment extends MapFragment implements GoogleApiClient.ConnectionCallbacks,
@@ -23,9 +44,35 @@ public class ClientMapFragment extends MapFragment implements GoogleApiClient.Co
     private Route currentSelectedRoute;
     private DatabaseReference currentRouteRef;
     private GoogleMap mMap;
+    private final String MAIN_TAG="main";
+    private final String REQUEST="request";
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this.getActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1600);
+
+        }
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest,
+                    this);
+        } else {
+            handleNewLocation(location);
+        }
+
+    }
+
+    private void handleNewLocation(Location location) {
+        final double currentLatitude = location.getLatitude();
+        final double currentLongitude = location.getLongitude();
+        LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 16));
 
     }
 
@@ -41,11 +88,63 @@ public class ClientMapFragment extends MapFragment implements GoogleApiClient.Co
 
     @Override
     public void onLocationChanged(Location location) {
-
+        handleNewLocation(location);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1600);
+
+        }
+        mMap.setMyLocationEnabled(true);
+        if (REQUEST.equals(getTag())){
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        mGoogleApiClient = new GoogleApiClient.Builder(this.getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000);
+
+        getMapAsync(this);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this.getActivity()
+        );
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+
+        if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1600);
+        }
+        LocationServices.getFusedLocationProviderClient(this.getActivity()).
+                requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                }, Looper.myLooper());
+        return super.onCreateView(layoutInflater, viewGroup, bundle);
     }
 }
